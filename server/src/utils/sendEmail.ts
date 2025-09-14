b6aa689521c3c8096c1bcdb0bddffa253670d771
syedomer17 <syedomerali2006@gmail.com>
2025-09-14 15:47:28 +0530

    Updated by Omer on 2025-09-14 at 03:47PM

---
+++ server/src/utils/sendEmail.ts
@@ -0,0 +1,25 @@
+import nodemailer from 'nodemailer';
+import env from '../config/env';
+
+export const sendEmail = async (to: string, subject: string, text: string): Promise<void> => {
+  try {
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: env.EMAIL_USER,
+        pass: env.EMAIL_PASSWORD,
+      },
+    });
+
+    await transporter.sendMail({
+      from: env.EMAIL_USER,
+      to,
+      subject,
+      text,
+    });
+
+    console.log("Email sent successfully");
+  } catch (error) {
+    console.error("Email not sent:", error);
+  }
+};
